abd2ea59d06adbb120289014f6b8336efa7bf916
Hussein Aisak <85851097+Huss3n@users.noreply.github.com>
2024-01-31 19:39:28 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
+# Bash script that takes in a URL, sends a request to that URL to the project
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
